f25ad1caebe3938ed500602ba11317ee0c132584
jonasprodrigues <jonas2r2@gmail.com>
2023-10-13 16:25:37 -0300

    funcao scroll css e js

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,14 +1,37 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkboxes);
+
+checkboxes()
+
+function checkboxes() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+        
+        if(boxTop < triggerBottom) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    })
+}
+
+
+/*
 const observer = new IntersectionObserver(entries => {
     console.log(entries)
 
-    Array.from(entries).forEach(entry =>
+    Array.from(entries).forEach(entry => {
         if (entry.intersectionRatio >= 1) {
             entry.target.classList.add('init-hidden-off')
-        })
+        }
+    })
 }, {
     threshold: [0, .5, 1]
 })
 
-Array.from(document.querySelectorAll('.init-hidden')).forEach(Element => {
-    observer.observe(Element)
+Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
+    observer.observe(element)
 })
+*/
