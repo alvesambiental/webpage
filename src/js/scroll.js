f7ac997582c82efccf76714c36b904efbe6999aa
jonasprodrigues <jonas2r2@gmail.com>
2023-10-13 06:38:56 -0300

    scroll.js

---
+++ src/js/scroll.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(entries => {
+    console.log(entries)
+
+    Array.from(entries).forEach(entry =>
+        if (entry.intersectionRatio >= 1) {
+            entry.target.classList.add('init-hidden-off')
+        })
+}, {
+    threshold: [0, .5, 1]
+})
+
+Array.from(document.querySelectorAll('.init-hidden')).forEach(Element => {
+    observer.observe(Element)
+})
